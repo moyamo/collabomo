ac6af97c461c46cd2e351505592b346754683f65
moyamo <moyamodehacker@gmail.com>
2014-10-17 19:58:59 +0200

    Add session and authentication
    Collabomo reads the secret session key, member names and the password
    from the secrets file. It makes sure the user uses the correct password
    and username.

--- omo-collab.py
+++ omo-collab.py
@@ -1,21 +1,44 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
 
 @app.route('/collabomo')
 def collabomo():
-    return render_template('omo_collab.html')
+    if 'username' in session and session['username'] in members:
+        return render_template('omo_collab.html')
+    else:
+        return 'You are not allowed to view this'
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', app_name="Collabomo")
+    if request.method == 'POST':
+        if ('password' in request.form and
+                'username' in request.form and
+                request.form['password'] == password and
+                request.form['username'] in members):
+            username = request.form['username']
+            session['username'] = username
+            return redirect(url_for('collabomo'))
+        else:
+            return render_template('login.html', app_name="Collabomo Access Denied")
+    else:
+        return render_template('login.html', app_name="Collabomo")
 
 @app.route('/')
 def index():
     return redirect(url_for('login'))
+
+members = list()
+password = ''
+with open('secrets') as f:
+    secret = f.readline()
+    app.secret_key = secret
+    for i in range(4):
+        members.append(f.readline().strip())
+    password = f.readline().strip()
 
 if __name__ == '__main__':
     app.run(debug=True)
