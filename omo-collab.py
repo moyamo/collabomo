9d1f26d8f974776c0ebd0dca41bbf4eb8046ffbf
moyamo <moyamodehacker@gmail.com>
2014-10-17 19:22:01 +0200

    Create login page
    Create a bare-bones login page with username and password field.

--- omo-collab.py
+++ omo-collab.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/collabomo')
+def collabomo():
+    return render_template('omo_collab.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html', app_name="Collabomo")
+
 @app.route('/')
 def index():
-    return render_template('omo_collab.html')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
