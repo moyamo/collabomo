f3b8aa707fc69ba5ebc62f3eb868a1f576a62902
moyamo <moyamodehacker@gmail.com>
2014-10-17 20:37:24 +0200

    Fill collabomo.html with actual data

--- collabomo.py
+++ collabomo.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/collabomo')
 def collabomo():
     if 'username' in session and session['username'] in members:
-        return render_template('collabomo.html')
+        return render_template('collabomo.html', names = members, NUM_OF_QUESTIONS = NUM_OF_QUESTIONS, answers = answers)
     else:
         return 'You are not allowed to view this'
 
@@ -33,11 +33,15 @@ def index():
 
 members = list()
 password = ''
+answers = dict()
+NUM_OF_QUESTIONS = 20
 with open('secrets') as f:
     secret = f.readline()
     app.secret_key = secret
     for i in range(4):
         members.append(f.readline().strip())
+    for m in members:
+        answers[m] = [None for i in range(NUM_OF_QUESTIONS + 1)]
     password = f.readline().strip()
 
 if __name__ == '__main__':
