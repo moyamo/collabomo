cf77146a7ea9b5c5c56eb152931d03755aca5b95
moyamo <moyamodehacker@gmail.com>
2014-10-17 18:37:04 +0200

    Add main page of OMO collab

--- omo-collab.py
+++ omo-collab.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return render_template('omo_collab.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
